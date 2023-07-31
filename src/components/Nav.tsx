3233e2abed22befbdb8c170a3eedf5316222ad81
Nicholas-Vandyck <nicholas.vandyck@amalitech.com>
2023-07-31 11:53:16 +0000

    working on the header file

---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import "../styles/Nav.scss"
+import Toggle from "./Toggle";
+
+const Nav = () => {
+  return (
+    <header>
+    <h1>
+      Where in the world?
+    </h1>
+    < Toggle />
+  </header>
+  )
+}
+ 
+export default Nav;
+
---
+++ src/components/Toggle.tsx
@@ -0,0 +1,30 @@
+import { FaMoon, FaSun } from "react-icons/fa"
+import { useState, useEffect } from "react"
+
+
+
+const Toggle = () => {
+    const storedDarkMode = localStorage.getItem('darkMode');
+    const [darkMode, setDarkMode] = useState(storedDarkMode ? JSON.parse(storedDarkMode) : false);
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+      };
+
+      useEffect(() => {
+        localStorage.setItem('darkMode', JSON.stringify(darkMode));
+        if (darkMode) {
+          document.body.classList.add('dark-mode');
+        } else {
+          document.body.classList.remove('dark-mode');
+        }
+        }, [darkMode]);
+
+    return ( 
+        <button className="toggle-button" onClick={toggleDarkMode}>
+        {darkMode ? <FaSun className="icon" /> : <FaMoon className="icon" />}
+        <span className="mode-text">{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
+      </button>
+    )
+}
+ 
+export default Toggle;
